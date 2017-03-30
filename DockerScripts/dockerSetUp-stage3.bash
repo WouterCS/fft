497d5e1f93ef4c71766c5e5cdfd7818ced3bf3e7
Wouter Uijens <wouter.cs@gmail.com>
2017-03-30 15:46:57 +0200

    added git pull message in stage-3 script

--- DockerScripts/dockerSetUp-stage3.bash
+++ DockerScripts/dockerSetUp-stage3.bash
@@ -5,6 +5,8 @@ PROJECTPATH="$(dirname "$(dirname "${BASH_SOURCE[0]}")")" #'/home/wouter/Documen
 docker rm $(docker stop $(docker ps -aq --no-trunc))
 nvidia-docker run -itd -p 8888:8888 -p 6006:6006 --name $NAME -v $PROJECTPATH/notebooks:/notebooks -v $PROJECTPATH/RFNN:/usr/local/lib/python2.7/dist-packages/RFNN $IMAGENAME 
 
+cd $PROJECTPATH
+git pull
 docker cp $PROJECTPATH/RFNN     $NAME:/RFNN
 docker cp $PROJECTPATH/DockerScripts/DockerCreation $NAME:/scripts
 docker start $NAME 
