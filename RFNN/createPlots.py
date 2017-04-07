7bd66f46450ebebcd06efa774414709e2fd124e3
Wouter Uijens <wouter.cs@gmail.com>
2017-04-07 12:45:17 +0200

    Update script to include logging (attempt 1)

--- RFNN/createPlots.py
+++ RFNN/createPlots.py
@@ -6,13 +6,13 @@ def createPlot(folders, targetName, labels = map(lambda x: 'label %d' % x, range
     colors = ['red', 'green', 'blue', 'black', 'yellow', 'purple', 'orange', 'grey', 'pink']
     plt.clf()
     for i in range(len(folders)):
-        lpara = para.parameters('/home/wouter/Dropbox/thesis/results/%s/para' % folders[i]); plt.plot(lpara.acc_train, lpara.acc_test, color = colors[i], label = labels[i])
+        lpara = para.parameters('/home/wouter/Dropbox/thesis/stored_results/results-allSizes-adadelta-3-0.03/%s/para' % folders[i]); plt.plot(lpara.acc_test[-100:], color = colors[i], label = labels[i])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.legend(prop={'size':10})
-    plt.xscale('log')
-    plt.yscale('log')
+    #plt.xscale('log')
+    #plt.yscale('log')
     plt.figure(num =1, figsize = (20,20), dpi = 800)
     plt.savefig('/home/wouter/Dropbox/thesis/%s.jpg' % targetName)
 
@@ -22,5 +22,6 @@ def createPlot(folders, targetName, labels = map(lambda x: 'label %d' % x, range
 # lpara = para.parameters('/home/wouter/Documents/DockerMap/results/results-300-1/para'); plt.plot(lpara.acc_test, color = (0.5,0,0), label = '300 trainingcases - ADAM -lr 10^-4')
 # lpara = para.parameters('/home/wouter/Documents/DockerMap/results-300-1000-abs-adam/results-300-1/para'); plt.plot(lpara.acc_test, color = (0.5,0.5,0.5), label = '300 trainingcases - ADAM - lr 10^0')
  
- 
- 
+ #reload(RFNN.createPlots);from RFNN.createPlots import createPlot
+ #
+#createPlot(map(lambda x: 'results-%d00-1' % x, [50,100,200,600]), 'test',map(lambda x: 'Number of examples: %d00' % x, [3,10,20,50,100,200,600]), 'Adadelta 3 to 3e-2', xlabel = 'Epochs', ylabel = 'test error-rate')
