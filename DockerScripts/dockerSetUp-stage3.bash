33834d11af0013cab95ea9a51a93eb121029419d
Wouter Uijens <wouter.cs@gmail.com>
2017-04-24 13:39:39 +0200

    First save reults loca;lly (not dropbox), to prevent lock errors

--- DockerScripts/dockerSetUp-stage3.bash
+++ DockerScripts/dockerSetUp-stage3.bash
@@ -1,11 +1,12 @@
 NAME='tfGPU'
 IMAGENAME='myimage2' # 'mydockerimage' #gcr.io/tensorflow/tensorflow:latest-gpu /bin/bash
+LOCALPATH='/home/uijenswr/Documents/tempResults'
 DROPBOXPATH='/home/uijenswr/Dropbox/thesis'
 
 
 PROJECTPATH="$(dirname "$(dirname "${BASH_SOURCE[0]}")")" 
 docker rm $(docker stop $(docker ps -aq --no-trunc))
-nvidia-docker run -itd -p 8888:8888 -p 6006:6006 --name $NAME -v $DROPBOXPATH/results:/results -v $PROJECTPATH/RFNN:/usr/local/lib/python2.7/dist-packages/RFNN $IMAGENAME 
+nvidia-docker run -itd -p 8888:8888 -p 6006:6006 --name $NAME -v $LOCALPATH/results:/results -v $PROJECTPATH/RFNN:/usr/local/lib/python2.7/dist-packages/RFNN $IMAGENAME 
 
 cd $PROJECTPATH
 git pull
@@ -13,5 +14,5 @@ docker cp $PROJECTPATH/RFNN     $NAME:/RFNN
 docker cp $PROJECTPATH/DockerScripts/DockerCreation $NAME:/scripts
 docker start $NAME 
 docker exec -it $NAME /bin/bash /scripts/ondockerSetUp.sh
-docker cp $NAME:/results                   $DROPBOXPATH
-docker exec -it $NAME /bin/bash
+docker cp $LOCALPATH              $DROPBOXPATH
+
