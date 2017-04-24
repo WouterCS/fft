f907369c30984946d171c4567a05eda9c83bddc5
Wouter Uijens <wouter.cs@gmail.com>
2017-04-24 13:44:03 +0200

    First save reults loca;lly (not dropbox), to prevent lock errors (attempt 2)

--- DockerScripts/DockerCreation/ondockerSetUp.sh
+++ DockerScripts/DockerCreation/ondockerSetUp.sh
@@ -2,7 +2,7 @@
 #rm -r /usr/local/lib/python2.7/dist-packages/tensorflow/python/user_ops
 #mv /user_ops /usr/local/lib/python2.7/dist-packages/tensorflow/python
 mv /RFNN /usr/local/lib/python2.7/dist-packages/
-#mkdir -p /results
+mkdir -p /results
 #touch /usr/local/lib/python2.7/dist-packages/tensorflow/python/user_ops/__init__.py
 #cd /usr/local/lib/python2.7/dist-packages/tensorflow/python/user_ops
 #g++ -std=c++11 -shared gaussian_basis_filters.cc njet.cc -o gaussian_basis_filters.so -fPIC -I $TF_INC -O2
