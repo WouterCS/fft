9f3c946ad98959795fd2aaffcd34d803845bfc95
Wouter Uijens <wouter.cs@gmail.com>
2017-05-12 16:20:59 +0200

    Set batchsize  to 25

--- RFNN/trainnonlin/parameters.py
+++ RFNN/trainnonlin/parameters.py
@@ -27,7 +27,7 @@ class parameters:
         
         
         # training parameters
-        self.batchsize = 2
+        self.batchsize = 25
         self.optimizer = 'adam'
         self.fixed_lr = True
         self.initial_lr = 1
