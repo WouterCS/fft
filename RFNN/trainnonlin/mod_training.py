41fa9dc7bebb78a7b7c143b0e74fe2e3193c1aed
Wouter Uijens <wouter.cs@gmail.com>
2017-05-12 13:39:38 +0200

    Added modified training file to find minimal example of error in optimizer. (attempt 11)

--- RFNN/trainnonlin/mod_training.py
+++ RFNN/trainnonlin/mod_training.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 def do_training():
-    # Create placeholders
+    # Create placeholders for potential training-data/labels
     train_data_node = tf.placeholder(tf.complex64,
                                      shape=(25, 10),
                                      name="train_data_node")
@@ -9,22 +9,20 @@ def do_training():
     train_labels_node = tf.placeholder(tf.complex64,
                                        shape=(25, 10),
                                        name="train_labels_node")
-     
+    
+    # create and initialise the weights
     weights = {
-        # Fully connected weights
         'fc_w1': tf.Variable(tf.complex( tf.random_normal([10, 10], stddev=0.01, dtype =  tf.float32),
                                                     tf.random_normal([10, 10], stddev=0.01, dtype =  tf.float32))),
         'fc_b1': tf.Variable(tf.complex( tf.random_normal([10]), tf.random_normal([10]))),
         }
     
     prediction = model(train_data_node, weights)
-    loss = tf.cast(tf.norm(prediction - train_labels_node), tf.float32)
+    loss = tf.real(tf.norm(prediction - train_labels_node))
 
     train_op = tf.train.AdamOptimizer(learning_rate=1.0).minimize(loss)
             
-            
 def model(data, weights):
-    
     l1 = tf.matmul(data, weights['fc_w1'])                                                        # FC
     l1 = l1 + weights['fc_b1']
     return l1
