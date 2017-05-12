fdf30d1cb87c4ef1cdbd9072a40a1646cb893a0a
Wouter Uijens <wouter.cs@gmail.com>
2017-05-12 13:18:07 +0200

    Added modified training file to find minimal example of error in optimizer. (attempt 9)

--- RFNN/trainnonlin/mod_training.py
+++ RFNN/trainnonlin/mod_training.py
@@ -11,21 +11,15 @@ def do_training():
                                        name="train_labels_node")
      
     weights = {
-
-        # Fully connected weights, layer 1
-        'fc_w1': tf.Variable(tf.complex( tf.random_normal([10, 10],
-                                                        stddev=0.01
-                                                        , dtype =  tf.float32),
-                                                    tf.random_normal([10, 10],
-                                                        stddev=0.01
-                                                        , dtype =  tf.float32))),
-        'fc_b1': tf.Variable(tf.complex(tf.random_normal([10]), tf.random_normal([10]))),
+        # Fully connected weights
+        'fc_w1': tf.Variable(tf.complex( tf.random_normal([10, 10], stddev=0.01, dtype =  tf.float32),
+                                                    tf.random_normal([10, 10], stddev=0.01, dtype =  tf.float32))),
+        'fc_b1': tf.Variable(tf.complex( tf.random_normal([10]), tf.random_normal([10]))),
         }
     
-    logits = model(train_data_node, weights)
-    loss = tf.real(tf.norm(logits - train_labels_node))
+    prediction = model(train_data_node, weights)
+    loss = tf.real(tf.norm(prediction - train_labels_node))
 
-    
     train_op = tf.train.AdamOptimizer(learning_rate=1.0).minimize(loss)
             
             
@@ -34,6 +28,3 @@ def model(data, weights):
     l1 = tf.matmul(data, weights['fc_w1'])                                                        # FC
     l1 = l1 + weights['fc_b1']
     return l1
-
-
-
