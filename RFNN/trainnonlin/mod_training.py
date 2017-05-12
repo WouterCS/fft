cfd4d3d3956ee60c964881325174c99fbbec955a
Wouter Uijens <wouter.cs@gmail.com>
2017-05-12 12:57:17 +0200

    Added modified training file to find minimal example of error in optimizer. (attempt 6)

--- RFNN/trainnonlin/mod_training.py
+++ RFNN/trainnonlin/mod_training.py
@@ -14,14 +14,6 @@ def test_do_training(path): # '/home/wouter/Documents/git/fft/RFNN/trainnonlin/s
 
 def do_training(params, dataset):
     print('Do training: %s'  % str(datetime.now()))
-    
-    # Shorten some variable names
-    train_data          = dataset['training_set']['data']
-    train_labels        = dataset['training_set']['labels']
-
-    # reshape to combine the MNIST-examples dimension and the kernal-dimension
-    train_data = train_data.reshape((train_data.shape[0] * train_data.shape[1], train_data.shape[2], train_data.shape[3], train_data.shape[4]))
-    train_labels = train_labels.reshape((train_labels.shape[0] * train_labels.shape[1], train_labels.shape[2], train_labels.shape[3], train_labels.shape[4]))
 
     # Set the random seed
     tf.set_random_seed(params.seed)
@@ -42,7 +34,6 @@ def do_training(params, dataset):
                                        name="train_labels_node")
      
     sizeImage = dataset['height'] * dataset['width'] * dataset['depth']
-        
     weights = {
 
         # Fully connected weights, layer 1
@@ -63,9 +54,6 @@ def do_training(params, dataset):
             
             
 def model(params, data, weights, train=False):
-    
-    # Dropout parameters
-    KEEP_PROB_HIDDEN = params.KEEP_PROB_HIDDEN
     shape = data.get_shape().as_list()
     
     l1 = tf.reshape(data, [shape[0], shape[1] * shape[2] * shape[3]])
