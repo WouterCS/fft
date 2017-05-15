88b0baf97b09d2b025dc3e9caa825688c00e77b7
Wouter Uijens <wouter.cs@gmail.com>
2017-05-15 20:36:29 +0200

    Added first real model test. (attempt 78)

--- RFNN/trainnonlin/training.py
+++ RFNN/trainnonlin/training.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 def test_do_training(path): # '/home/wouter/Documents/git/fft/RFNN/trainnonlin/storedData.npz')
     print('start')
-    for n in dir(tf)
+    for n in dir(tf):
         print(n)
     dataset = loadData(path)
     params = para.parameters('/home/wouter/Documents/git/fft/RFNN/trainnonlin/para')
