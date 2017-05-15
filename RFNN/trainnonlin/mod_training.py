a9092510891ca1ab6573b269815f381077d85e78
Wouter Uijens <wouter.cs@gmail.com>
2017-05-15 19:15:06 +0200

    Added first real model test. (attempt 72)

--- RFNN/trainnonlin/mod_training.py
+++ RFNN/trainnonlin/mod_training.py
@@ -47,7 +47,7 @@ def apply_model():
     new_saver = tf.train.import_meta_graph('/results/weights.meta')
     new_saver.restore(sess, tf.train.latest_checkpoint('/results'))
     
-    print(model(np.random.random(5).astype(np.float32), weights))
+    print(model(np.random.random(5).astype(np.float32), weights).eval(session=sess))
             
             
 def model(data, weights):
