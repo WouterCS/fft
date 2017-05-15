6c9ee840e35fa762dd7f46ee673226afe9c8bd59
Wouter Uijens <wouter.cs@gmail.com>
2017-05-15 12:17:07 +0200

    Added first real model test. (attempt 7)

--- RFNN/trainnonlin/training.py
+++ RFNN/trainnonlin/training.py
@@ -114,7 +114,7 @@ def do_training(params, dataset):
         }
     
     error = model(params, train_data_node, weights, True) - train_labels_node
-    print(str([error.shape[0] * error.shape[1], error.shape[2] * error.shape[3]]))
+    print(dir(error.shape[0] ))
     a = tf.reshape(error, [error.shape[0] * error.shape[1], error.shape[2] * error.shape[3]])
     b = tf.norm(a, axis = 1)
     c = tf.real(b)
