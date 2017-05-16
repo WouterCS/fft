60275d8768ea435871d48d2aecf7112fa7f491ec
Wouter Uijens <wouter.cs@gmail.com>
2017-05-16 11:34:18 +0200

    Added first real model test. (attempt 97)

--- RFNN/trainnonlin/training.py
+++ RFNN/trainnonlin/training.py
@@ -45,11 +45,12 @@ def checkLossForTestSet(weights, params, testSet, sess, prediction, train_data_n
         input = np.fft.rfft2(randomImage).astype('complex64', casting = 'same_kind')
         groundTruth = np.fft.rfft2(np.maximum(randomImage, 0)).astype('complex64', casting = 'same_kind')
         p = sess.run([prediction],feed_dict={train_data_node:input})
-        loss = np.mean(np.absolute(p - groundTruth))#.eval(session=sess)
-        if loss > 100:
-            print('Loss is %f for image:' % loss)
-            print(randomImage)
-        storedLoss.append(loss)
+        loss = np.mean(np.absolute(p - groundTruth), axis = [1,2,3])#.eval(session=sess)
+        for j in len(loss)
+            if loss[j] > 100
+                print('Loss is %f for image:' % loss[j])
+                print(randomImage[j])
+        storedLoss = np.concatenate((storedLoss,loss))
     print('Max loss: %f, average loss: %f' % (np.max(storedLoss), np.mean(storedLoss)))
     
     
