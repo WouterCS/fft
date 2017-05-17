ff40e9fa31cbe49b6a0afa232dd19455046a1513
Wouter Uijens <wouter.cs@gmail.com>
2017-05-17 17:22:10 +0200

    Added first real model test. (attempt 113)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -9,4 +9,4 @@ g++ -std=c++11 -shared gaussian_basis_filters.cc njet.cc -o gaussian_basis_filte
 g++ -std=c++11 -shared gaussian_basis_filters_backprop_sigma.cc njet.cc -o gaussian_basis_filters_backprop_sigma.so -fPIC -I $TF_INC -O2
 
 
-python -c "import RFNN.trainnonlin.generateData as gd; gd.generateDataFixedArg()"
+python -c "import RFNN.trainnonlin.generateData as gd; gd.run()"
