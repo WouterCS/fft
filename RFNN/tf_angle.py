a3056249da75707ede60da8ba49e854289a64658
Wouter Uijens <wouter.cs@gmail.com>
2017-06-16 13:43:44 +0200

    Try to sqrt the magnitude of the Fourier coeffs as non-linearity (attempt 32)

---
+++ RFNN/tf_angle.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.python.framework import ops
+import numpy as np
+
+def np_atan2(x,y):
+    return np.arctan2(x, y).astype(np.float32)
+
+def atan2grad(op, grad):
+    y = op.inputs[0]
+    x = op.inputs[1]
+    
+    if x == 0 and y == 0:
+        return 0, 0
+    
+    return grad * (x / (x^2 + y^2)), grad * (y / (x^2 + y^2)) #the propagated gradient with respect to the first and second argument respectively
+
+def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
+
+    # Need to generate a unique name to avoid duplicates:
+    rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+8))
+
+    tf.RegisterGradient(rnd_name)(grad)
+    g = tf.get_default_graph()
+    with g.gradient_override_map({"PyFunc": rnd_name}):
+        return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
+
+
+def tf_atan2(x,y, name=None):
+
+    with ops.name_scope("atan2", name, [x,y]) as name:
+        z = py_func(np_atan2,
+                        [x,y],
+                        [tf.float32],
+                        name=name,
+                        grad=atan2grad)
+        return z[0]
+    
+def tf_angle(c):
+    return tf_atan2(tf.imag(c), tf.real(c))
