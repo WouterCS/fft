7e534610addd91da9ebb907395dbb0e404eca68c
Wouter Uijens <wouter.cs@gmail.com>
2017-06-23 18:14:04 +0200

    Locally change sqrt-magnitude to custom sigmoid op (attempt1)

--- RFNN/experiment.py
+++ RFNN/experiment.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from RFNN.datasets.utils import show_samples, shuffle_samples, split_dataset
 from tf_angle import sqrtMagnitude, tf_angle
-from test_customop import tf_abs
+from test_customop import tf_sigmoid
 
 #fft relu
 from tensorflow.python.ops.spectral_ops import rfft2d, rfft
@@ -168,7 +168,7 @@ def fftReLu(layerIn, params):
         layerIn = tf.transpose(layerIn, [0, 3, 2, 1])
         printShape(layerIn.shape)
         #layerOut = irfft2d( sqrtMagnitude(rfft2d(layerIn) ))
-        layerOut = irfft2d( tf.cast(tf_abs(rfft2d(layerIn) ), tf.complex64))
+        layerOut = tf_sigmoid(layerIn)
         printShape(layerOut.shape)
         layerOut = tf.transpose(layerOut, [0, 2, 3, 1])
         printShape(layerOut.shape)
--- RFNN/test_customop.py
+++ RFNN/test_customop.py
@@ -3,22 +3,18 @@ from tensorflow.python.framework import ops
 import numpy as np
 
 # wrapper for the numpy arctan2 function, where we make sure the output is of type float32
-def custom_op(x,y):
-    return (x*x + y*y).astype(np.float32)
+def custom_op(x):
+    return (1 / (1 + np.exp(-x))).astype(np.float32)
 
 # Function that can propegate the gradient through an arctan2 operation, in the format expected by Tensorflow
 def custom_op_grad(op, grad):
-    y = op.inputs[0]
-    x = op.inputs[1]
+    x = op.inputs[0]
     
-    if x == 0 and y == 0:
-        return 0, 0
+    sigmoidX = (1 / (1 + tf.exp(-x)))
+    return grad * (sigmoidX *(1 - sigmoidX))
     
-    return grad * (2*y), grad * (2*x) #the propagated gradient with respect to the first and second argument respectively
-
 # Wrapper around tf.py_func which allows gradients to be added.
 def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
-
     # Need to generate a unique name to avoid duplicates:
     rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+8))
 
@@ -28,11 +24,11 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
         return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
 
 # Tensorflow op that combines the arctan2 wrapper and the gradient function using  py_func to create a tensorflow op that performs the arctan2 function on pairs of tensors. 
-def tf_custom_op(x,y, name=None):
+def tf_custom_op(x, name=None):
 
-    with ops.name_scope("atan2", name, [x,y]) as name:
+    with ops.name_scope("atan2", name, [x]) as name:
         z = py_func(custom_op,
-                        [x,y],
+                        [x],
                         [tf.float32],
                         name=name,
                         grad=custom_op_grad)
@@ -41,5 +37,5 @@ def tf_custom_op(x,y, name=None):
         return z
     
 # Uses the arctan2 tensorflow op defined in this file to create an angle function, working on complex tensors.
-def tf_abs(c):
-    return tf.sqrt(tf_custom_op(tf.imag(c), tf.real(c)))
+def tf_sigmoid(x):
+    return tf_custom_op(x)
