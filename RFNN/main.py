553a02bbcf6f80a7adf98618caad795508559343
Wouter Uijens <wouter.cs@gmail.com>
2017-06-27 13:28:03 +0200

    Compare tensorflow abs to custom abs (attempt 3)

--- RFNN/main.py
+++ RFNN/main.py
@@ -37,8 +37,8 @@ def train():
     hyperParam.fftFunction = 'sqt-magnitude'
     trainGivenSetSize(dataset, hyperParam, 1)
     
-    #hyperParam.fftFunction = 'reference_op'
-    #trainGivenSetSize(dataset, hyperParam, 2)
+    hyperParam.fftFunction = 'reference_op'
+    trainGivenSetSize(dataset, hyperParam, 2)
 
     print('finished all training')
 
