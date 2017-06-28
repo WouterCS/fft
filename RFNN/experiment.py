cab867e8e2e6d129d79bdb83421d2c44d1455191
Wouter Uijens <wouter.cs@gmail.com>
2017-06-28 13:22:26 +0200

    Compare spatial abs with spatial sqrt of abs

--- RFNN/experiment.py
+++ RFNN/experiment.py
@@ -173,7 +173,7 @@ def fftReLu(layerIn, params):
         printShape(layerOut.shape)
         return layerOut
     if params.fftFunction == 'custom_op':
-        layerOut = tf.sqrt(tf.nn.relu(layerIn))
+        layerOut = tf.abs(layerIn) #tf.sqrt(tf.nn.relu(layerIn))
         #layerIn = tf.transpose(layerIn, [0, 3, 2, 1])
         #layerOut = irfft2d(tf.cast(tf.sqrt(tf.abs(rfft2d(layerIn))), tf.complex64))
         #layerOut = tf.transpose(layerOut, [0, 2, 3, 1])
