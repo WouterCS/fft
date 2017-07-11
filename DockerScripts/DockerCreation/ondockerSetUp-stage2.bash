a71fc6ed336a1c3cda739470b87b14a4cd6c113c
Wouter Uijens <wouter.cs@gmail.com>
2017-07-11 11:56:25 +0200

    Fixing problems when updating tensorflow to 1.2 (attempt 2)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -5,8 +5,8 @@ mv /user_ops /usr/local/lib/python2.7/dist-packages/tensorflow/python
 #mkdir -p /results
 #touch /usr/local/lib/python2.7/dist-packages/tensorflow/python/user_ops/__init__.py
 cd /usr/local/lib/python2.7/dist-packages/tensorflow/python/user_ops
-g++ -std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0 -shared gaussian_basis_filters.cc njet.cc -o gaussian_basis_filters.so -fPIC -I $TF_INC -O2
-g++ -std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0 -shared gaussian_basis_filters_backprop_sigma.cc njet.cc -o gaussian_basis_filters_backprop_sigma.so -fPIC -I $TF_INC -O2
-
+g++ -shared gaussian_basis_filters.cc njet.cc -o gaussian_basis_filters.so -fPIC -I $TF_INC -O2
+g++ -shared gaussian_basis_filters_backprop_sigma.cc njet.cc -o gaussian_basis_filters_backprop_sigma.so -fPIC -I $TF_INC -O2
+#std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0 
 
 #python -c "import RFNN.main as m; m.train()"
